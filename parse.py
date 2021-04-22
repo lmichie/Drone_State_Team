a8c022d46956ad4e5153010b541cc050f10479bc
Lindsey Michie <lmichie@nd.edu>
2021-04-21 21:47:52 -0400

    Parameter name and value stored in dict

--- parse.py
+++ parse.py
@@ -3,9 +3,16 @@ import sys
 import re
 import os
 import requests
+import pprint
 
 url = 'https://yld.me/raw/bUAw.txt'
 data = {}
 
 response = requests.get(url)
-print(response.text)
+log = response.text
+for line in log.split('}'):
+	file_line = line[26:]
+	if(file_line.startswith("PARM")):
+		parm_name = (file_line.split(':')[2]).split(',')[0].lstrip()
+		data[parm_name] = float(file_line.split(':')[3].lstrip())
+pprint.pprint(data)
