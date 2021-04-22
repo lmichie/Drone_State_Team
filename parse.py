2167a813da889a55341c2b4237c96af218b0f7f8
Lindsey Michie <lmichie@nd.edu>
2021-04-22 18:59:35 -0400

    Fixed Dictionary Values

--- parse.py
+++ parse.py
@@ -14,9 +14,11 @@ log = response.text
 for line in log.split('}'):
 	file_line = line[26:]
 	if(file_line.startswith("PARM")):
+		temp_data = {}
 		parm_name = (file_line.split(':')[2]).split(',')[0].lstrip()
-		data[parm_name]["Value"] = float(file_line.split(':')[3].lstrip())
-		data[parm_name]["Time"] = (file_line.split(':')[1]).split(',')[0].lstrip()
+		temp_data["value"] = float(file_line.split(':')[3].lstrip())
+		temp_data["time"] = (file_line.split(':')[1]).split(',')[0].lstrip()
+		data[parm_name] = temp_data
 	
 new_structure["PARM"] = data
 pprint.pprint(new_structure)
