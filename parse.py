8da084ed3b238e7d893448aa6cb74343e78e3828
Lindsey Michie <lmichie@nd.edu>
2021-04-22 10:53:31 -0400

    Nested dictionary - KeyError

--- parse.py
+++ parse.py
@@ -6,6 +6,7 @@ import requests
 import pprint
 
 url = 'https://yld.me/raw/bUAw.txt'
+new_structure = {}
 data = {}
 
 response = requests.get(url)
@@ -14,5 +15,8 @@ for line in log.split('}'):
 	file_line = line[26:]
 	if(file_line.startswith("PARM")):
 		parm_name = (file_line.split(':')[2]).split(',')[0].lstrip()
-		data[parm_name] = float(file_line.split(':')[3].lstrip())
-pprint.pprint(data)
+		data[parm_name]["Value"] = float(file_line.split(':')[3].lstrip())
+		data[parm_name]["Time"] = (file_line.split(':')[1]).split(',')[0].lstrip()
+	
+new_structure["PARM"] = data
+pprint.pprint(new_structure)
