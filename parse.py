d62062bd446ea7807d411f633fe3697800919809
Lindsey Michie <lmichie@nd.edu>
2021-04-25 20:43:39 -0400

    FLT

--- parse.py
+++ parse.py
@@ -16,9 +16,10 @@ for line in log.split('}'):
 	if(file_line.startswith("PARM")):
 		temp_data = {}
 		parm_name = (file_line.split(':')[2]).split(',')[0].lstrip()
-		temp_data["value"] = float(file_line.split(':')[3].lstrip())
-		temp_data["time"] = (file_line.split(':')[1]).split(',')[0].lstrip()
-		data[parm_name] = temp_data
+		if(parm_name.startswith("FLT")):
+			temp_data["value"] = float(file_line.split(':')[3].lstrip())
+			temp_data["time"] = (file_line.split(':')[1]).split(',')[0].lstrip()
+			data[parm_name] = temp_data
 	
-new_structure["PARM"] = data
+new_structure["PARM"] = [data]
 pprint.pprint(new_structure)
