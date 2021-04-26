2531e116f624ef62ab903ea136ec2e567eb86870
Lindsey Michie <lmichie@nd.edu>
2021-04-25 21:26:16 -0400

    Data structure reflects visual initial set up

--- parse.py
+++ parse.py
@@ -8,6 +8,8 @@ import pprint
 url = 'https://yld.me/raw/bUAw.txt'
 new_structure = {}
 data = {}
+data2 = {}
+list_alt = []	
 
 response = requests.get(url)
 log = response.text
@@ -20,6 +22,10 @@ for line in log.split('}'):
 			temp_data["value"] = float(file_line.split(':')[3].lstrip())
 			temp_data["time"] = (file_line.split(':')[1]).split(',')[0].lstrip()
 			data[parm_name] = temp_data
-	
-new_structure["PARM"] = [data]
+	if(file_line.startswith("CMD")):
+		list_alt.append(float(file_line.split(':')[11].lstrip()))
+
+data2["Altitude"] = list_alt
+new_structure["Flight Modes"] = data
+new_structure["Parameters"] = data2
 pprint.pprint(new_structure)
