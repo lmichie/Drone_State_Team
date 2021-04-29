5781d533a81e784935518f0c6fd187bd11549cac
ColinFang <wfang@nd.edu>
2021-04-29 21:39:54 +0800

    add time to altitude

--- parse.py
+++ parse.py
@@ -9,7 +9,8 @@ url = 'https://yld.me/raw/bUAw.txt'
 new_structure = {}
 data = {}
 data2 = {}
-list_alt = []	
+list_alt = []
+list_time = []
 
 response = requests.get(url)
 log = response.text
@@ -24,8 +25,15 @@ for line in log.split('}'):
 			data[parm_name] = temp_data
 	if(file_line.startswith("CMD")):
 		list_alt.append(float(file_line.split(':')[11].lstrip()))
+		# Add a list of time corresponding to the altitude value
+		str = [letter for letter in file_line.split(':')[1] if letter.isnumeric()]
+		str1 = ''.join(str)
+		list_time.append(float(str1))
 
-data2["Altitude"] = list_alt
+data2["Altitude"] = {
+    "value":list_alt,
+    "time":list_time,
+}
 new_structure["Flight Modes"] = data
 new_structure["Parameters"] = data2
 pprint.pprint(new_structure)
