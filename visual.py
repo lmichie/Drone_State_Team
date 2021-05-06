0df6eb1b69bcd298906f6a62e929cf3263fa741b
ColinFang <wfang@nd.edu>
2021-05-07 02:42:39 +0800

    Cleaned the structure a bit

--- visual.py
+++ visual.py
@@ -6,7 +6,14 @@ import parse
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 
+# Globals
+path_drone = "drone.jpg"
+alt = "Altitude.JPG"
+volt = "Battery.JPG"
+temperature = "Temperature.JPG"
 DATA = parse.parse()
+
+# Functions
 def plot(var):  
     # the figure that will contain the plot
     fig = Figure(figsize = (5, 5),dpi = 100)
@@ -25,29 +32,6 @@ def plot(var):
     plot1.plot(x_var, y_var, label = var)
     plot1.set(xlabel='Time', ylabel='Values', title='Graph')
     return fig
-
-# Uppercase
-path_drone = "drone.jpg"
-alt = "Altitude.JPG"
-volt = "Battery.JPG"
-temperature = "Temperature.JPG"
-
-
-
-# Make an tkinter window instance Tk()
-window = Tk()
-
-# Give a title to your tkinter window
-window.title("Drone State")
-
-# define the size of your tkinter window (mutable) "widthxheight"
-window.geometry("500x500")
-
-# define the minimum width and height of your window (width, height)
-window.minsize(500,500)
-
-# define the maximum width and height (width, height)
-window.maxsize(1000,1000) 
 
 def draw():
     var = var_entry.get()
@@ -88,42 +72,59 @@ def draw():
             break
     if not check:
         messagebox.showerror("Error",f"There is no variable {var_bg}")
-    
-# configure a color
-window.config(bg='white')
 
-# Adding a label to your window
-# Features = background, foreground, borderwidth, relief, font(font, fontsize,..)
-label1 = Label(window,text="Visualization of Flight Mode", bg="black", fg="white", 
-                font=('Helvetica',20,'bold'),#width=500, height=500, 
-                borderwidth=20,relief='sunken') #relief: proove,sunken,flat,raised,solid
-# Features of pack() anchor=(ne,nw,se,sw,n,e,w,s), fill=(X,Y,BOTH)
-# padx, pady ,ipadx, ipady - internal padding
-label1.pack(fill=X)
+# Main
+if __name__ == '__main__':
+    # Make an tkinter window instance Tk()
+    window = Tk()
 
-var_label = Label(window,text="Variable: ")
-var_label.pack(anchor='w')
+    # Give a title to your tkinter window
+    window.title("Drone State")
 
-# Adding an entry box to your window
-var_entry = Entry(window)
-var_entry.pack(anchor='w')
+    # define the size of your tkinter window (mutable) "widthxheight"
+    window.geometry("500x500")
 
-# Variable to be plotted against in the background
-var_label2 = Label(window,text="Background Variable: ")
-var_label2.pack(anchor='w')
-var_entry2 = Entry(window)
-var_entry2.pack(anchor='w')
+    # define the minimum width and height of your window (width, height)
+    window.minsize(500,500)
+
+    # define the maximum width and height (width, height)
+    window.maxsize(1000,1000) 
+        
+    # configure a color
+    window.config(bg='white')
+
+    # Adding a label to your window
+    # Features = background, foreground, borderwidth, relief, font(font, fontsize,..)
+    label1 = Label(window,text="Visualization of Flight Mode", bg="black", fg="white", 
+                    font=('Helvetica',20,'bold'),#width=500, height=500, 
+                    borderwidth=20,relief='sunken') #relief: proove,sunken,flat,raised,solid
+    # Features of pack() anchor=(ne,nw,se,sw,n,e,w,s), fill=(X,Y,BOTH)
+    # padx, pady ,ipadx, ipady - internal padding
+    label1.pack(fill=X)
+
+    var_label = Label(window,text="Variable: ")
+    var_label.pack(anchor='w')
+
+    # Adding an entry box to your window
+    var_entry = Entry(window)
+    var_entry.pack(anchor='w')
+
+    # Variable to be plotted against in the background
+    var_label2 = Label(window,text="Background Variable: ")
+    var_label2.pack(anchor='w')
+    var_entry2 = Entry(window)
+    var_entry2.pack(anchor='w')
 
 
-# Adding a button to your tkinter window
+    # Adding a button to your tkinter window
 
-Button1 = Button(window,text="Draw", command=draw)
-Button1.pack()
+    Button1 = Button(window,text="Draw", command=draw)
+    Button1.pack()
 
-image1 = Image.open(path_drone).resize((300,300), Image.ANTIALIAS)
-img = ImageTk.PhotoImage(image1)
-panel = Label(window, image = img)
-panel.pack(side = "bottom", fill="both",expand="yes")
+    image1 = Image.open(path_drone).resize((300,300), Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(image1)
+    panel = Label(window, image = img)
+    panel.pack(side = "bottom", fill="both",expand="yes")
 
-# Put it in a loop
-window.mainloop()
+    # Put it in a loop
+    window.mainloop()
