ff1b5054a2e22470a19c158607abb994dad16659
Lindsey Michie <lmichie@nd.edu>
2021-05-09 14:32:53 -0400

    setup.sh

---
+++ drone_state_team/setup.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/lmichie/Drone_State_Team/main/README.md?token=AQUX25SEGY4PD6E45ZP5XO3AUFSK2
+wget https://raw.githubusercontent.com/lmichie/Drone_State_Team/main/graph.py?token=AQUX25QITM5JJK33NRM2MMLAUFSQI
+wget https://raw.githubusercontent.com/lmichie/Drone_State_Team/main/parse.py?token=AQUX25VEYMGAQFZRRDWV23LAUFSRM
+wget https://raw.githubusercontent.com/lmichie/Drone_State_Team/main/visual.py?token=AQUX25SXG4A35DMXCZUWCGTAUFSSI
+
