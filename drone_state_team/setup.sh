acaa3b913a6acacaa7cc94ee5d4486aaa6b8df8a
Lindsey Michie <lmichie@nd.edu>
2021-05-09 20:45:20 -0400

    setup.sh

--- drone_state_team/setup.sh
+++ drone_state_team/setup.sh
@@ -1,4 +1,4 @@
-wget -O REAME.md https://raw.githubusercontent.com/lmichie/Drone_State_Team/main/README.md?token=AQUX25UBUASXGKHZFGEOZ2DAUG464
+wget -O README.md https://raw.githubusercontent.com/lmichie/Drone_State_Team/main/README.md?token=AQUX25UBUASXGKHZFGEOZ2DAUG464
 wget -O graph.py https://raw.githubusercontent.com/lmichie/Drone_State_Team/main/graph.py?token=AQUX25SVPJTBL3MIU4QBDF3AUG45O
 wget -O parse.py https://raw.githubusercontent.com/lmichie/Drone_State_Team/main/parse.py?token=AQUX25UNNN3SGFK54JVEHL3AUG42K
 wget -O visual.py https://raw.githubusercontent.com/lmichie/Drone_State_Team/main/visual.py?token=AQUX25SWLADW3JKNQEESLDDAUG44I
